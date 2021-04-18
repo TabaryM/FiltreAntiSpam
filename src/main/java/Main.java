2df71ecb7c602c8f41a4fc4ad8cb6557efdf54ec
adeleb54 <adeleb54.2@gmail.com>
2021-04-18 17:01:05 +0200

    Première implantation du test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import filtre.Filtre;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Combien de SPAM dans la base d'apprentissage ?");
+        int mSpam = scanner.nextInt();
+        System.out.println("Combien de HAM dans la base d'apprentissage ?");
+        int mHam = scanner.nextInt();
+
+        Filtre filtre = new Filtre();
+        System.out.println("Apprentissage...");
+        filtre.apprentissage(mSpam, mHam);
+
+        System.out.println("Test :");
+        filtre.test(Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[0]);
+
     }
 }
--- src/main/java/filtre/Filtre.java
+++ src/main/java/filtre/Filtre.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 public class Filtre {
     private final List<String> dictionnaire = new ArrayList<>();
-    private int[] message;
+    private int[] X;
+    private double[] probasSpam;
+    private double[] probasHam;
+    private double probaSpam;
+    private double probaHam;
 
     public void chargerDictionnaire(File file){
         dictionnaire.clear();
@@ -30,7 +34,7 @@ public class Filtre {
     }
 
     public void lireMessage(File file){
-        message = new int[1000];
+        X = new int[dictionnaire.size()];
         BufferedReader reader = null ;
         try {
             reader = new BufferedReader(new FileReader(file));
@@ -47,13 +51,103 @@ public class Filtre {
                 mots = ligne.split(" ");
                 for(String mot : mots){
                     if(dictionnaire.contains(mot)){
-                        message[dictionnaire.indexOf(mot)] = 1;
+                        X[dictionnaire.indexOf(mot)] = 1;
                     }
                 }
             }
         } catch (IOException e){
             e.printStackTrace();
             System.exit(-1);
+        }
+    }
+
+    public void apprentissage(int mSpam, int mHam){
+
+        probasSpam = new double[1000];
+        probasHam = new double[1000];
+        File file;
+
+        //Estimation des paramètres de distribution
+        for(int i=0; i<dictionnaire.size(); i++){
+
+            //SPAM
+            for(int j=0; j<mSpam; j++){
+                file = new File("baseapp/spam/"+j+".txt");
+                lireMessage(file);
+                if(X[i]==1){
+                    probasSpam[i]++;
+                }
+            }
+
+            //HAM
+            for(int j=0; j<mHam; j++){
+                file = new File("baseapp/ham/"+j+".txt");
+                lireMessage(file);
+                if(X[i]==1){
+                    probasHam[i]++;
+                }
+            }
+            probasSpam[i] = probasSpam[i]/mSpam;
+            probasHam[i] = probasHam[i]/mHam;
+        }
+
+        //Estimation des probabilités a priori
+        probaSpam = (double) mSpam/(mSpam+mHam);
+        probaHam = 1-probaSpam;
+
+    }
+
+    public void test(int mSpam, int mHam, String cheminTest){
+        testType(mSpam, cheminTest, true);
+        testType(mHam, cheminTest, false);
+    }
+
+    private void testType(int m, String cheminTest, boolean spam){
+
+        double probaPosterioriSpam;
+        double probaPosterioriHam;
+        String type;
+        if(spam)
+            type = "/spam/";
+        else
+            type = "/ham/";
+
+        File file;
+
+        for(int i = 0; i<m; i++){
+            probaPosterioriSpam = 1;
+            probaPosterioriHam = 1;
+
+            //Calcul des probabilités a posteriori
+            for(int j=0; j<dictionnaire.size(); j++){
+                file = new File(cheminTest+type+j+".txt");
+                lireMessage(file);
+                if(X[j] == 1) {
+                    probaPosterioriSpam *= probasSpam[j];
+                    probaPosterioriHam *= probasHam[j];
+                }
+                else {
+                    probaPosterioriSpam *= 1 - probasSpam[j];
+                    probaPosterioriHam *= 1 - probasHam[j];
+                }
+            }
+
+            probaPosterioriSpam *= probaSpam;
+            probaPosterioriHam *= probaHam;
+
+            //Évaluation
+            if(probaPosterioriSpam > probaPosterioriHam) {
+                System.out.print("Message " + i + " est prédit comme SPAM");
+                if(!spam)
+                    System.out.print("\t*** ERREUR ***");
+            }
+
+            else {
+                System.out.print("Message " + i + " est prédit comme HAM");
+                if(spam)
+                    System.out.print("\t*** ERREUR ***");
+            }
+            System.out.println();
         }
     }
 }
