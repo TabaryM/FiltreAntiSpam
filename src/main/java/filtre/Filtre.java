056b29c80c002115a4d27b79252087be94bc16df
TabaryM <mathieutab@free.fr>
2021-05-03 14:11:47 +0200

    Petit fix

--- src/main/java/filtre/Filtre.java
+++ src/main/java/filtre/Filtre.java
@@ -11,9 +11,15 @@ public class Filtre {
     private double[] probasHam;
     private double probaSpam;
     private double probaHam;
+    private final ClassLoader classLoader = getClass().getClassLoader();
+
+    public Filtre(){
+        chargerDictionnaire(loadRessource("dictionnaire1000en.txt"));
+    }
 
     public void chargerDictionnaire(File file){
         dictionnaire.clear();
+        System.out.println("Chargement du dictionnaire...");
         BufferedReader reader = null ;
         try {
             reader = new BufferedReader(new FileReader(file));
@@ -64,38 +70,45 @@ public class Filtre {
 
     public void apprentissage(int mSpam, int mHam){
 
-        probasSpam = new double[1000];
-        probasHam = new double[1000];
+        probasSpam = new double[dictionnaire.size()];
+        probasHam = new double[dictionnaire.size()];
         File file;
 
         //Estimation des paramètres de distribution
-        for(int i=0; i<dictionnaire.size(); i++){
-
-            //SPAM
-            for(int j=0; j<mSpam; j++){
-                file = new File("baseapp/spam/"+j+".txt");
-                lireMessage(file);
-                if(X[i]==1){
+        //SPAM
+        for(int j = 0; j<mSpam; j++) {
+            file = loadRessource("baseapp/spam/" + j + ".txt");
+//                file = new File("baseapp/spam/"+j+".txt");
+            lireMessage(file);
+            for (int i = 0; i < dictionnaire.size(); i++) {
+                if (X[i] == 1) {
                     probasSpam[i]++;
                 }
             }
+        }
 
-            //HAM
-            for(int j=0; j<mHam; j++){
-                file = new File("baseapp/ham/"+j+".txt");
-                lireMessage(file);
+        //HAM
+        for(int j = 0; j<mHam; j++){
+            file = loadRessource("baseapp/ham/"+j+".txt");
+//                file = new File("baseapp/ham/"+j+".txt");
+            lireMessage(file);
+            for (int i = 0; i < dictionnaire.size(); i++) {
                 if(X[i]==1){
                     probasHam[i]++;
                 }
             }
-            probasSpam[i] = probasSpam[i]/mSpam;
-            probasHam[i] = probasHam[i]/mHam;
+        }
+
+        for (int i = 0; i < dictionnaire.size(); i++) {
+            probasSpam[i] = probasSpam[i] / mSpam;
+            probasHam[i] = probasHam[i] / mHam;
         }
 
         //Estimation des probabilités a priori
         probaSpam = (double) mSpam/(mSpam+mHam);
         probaHam = 1-probaSpam;
 
+        System.out.printf("Proba a priori\tSpam : %.15f\tHam : %.15f\n", probaSpam, probaHam);
     }
 
     public void test(int mSpam, int mHam, String cheminTest){
@@ -104,7 +117,6 @@ public class Filtre {
     }
 
     private void testType(int m, String cheminTest, boolean spam){
-
         double probaPosterioriSpam;
         double probaPosterioriHam;
         String type;
@@ -116,13 +128,14 @@ public class Filtre {
         File file;
 
         for(int i = 0; i<m; i++){
-            probaPosterioriSpam = 1;
-            probaPosterioriHam = 1;
+            probaPosterioriSpam = probaSpam;
+            probaPosterioriHam = probaHam;
 
+//          file = new File(cheminTest+type+j+".txt");
+            file = loadRessource(cheminTest+type+i+".txt");
+            lireMessage(file);
             //Calcul des probabilités a posteriori
-            for(int j=0; j<dictionnaire.size(); j++){
-                file = new File(cheminTest+type+j+".txt");
-                lireMessage(file);
+            for(int j = 0; j<dictionnaire.size(); j++){
                 if(X[j] == 1) {
                     probaPosterioriSpam *= probasSpam[j];
                     probaPosterioriHam *= probasHam[j];
@@ -133,8 +146,10 @@ public class Filtre {
                 }
             }
 
-            probaPosterioriSpam *= probaSpam;
-            probaPosterioriHam *= probaHam;
+            System.out.printf("\nProba a priori\tSpam : %.15f\tHam : %.15f\n", probaSpam, probaHam);
+//            probaPosterioriSpam *= probaSpam;
+//            probaPosterioriHam *= probaHam;
+            System.out.printf("Proba a posteriori\tSpam : %.15f\tHam : %.15f\n", probaPosterioriSpam, probaPosterioriHam);
 
             //Évaluation
             if(probaPosterioriSpam > probaPosterioriHam) {
@@ -150,5 +165,9 @@ public class Filtre {
             }
             System.out.println();
         }
+    }
+
+    private File loadRessource(String URL){
+        return new File(classLoader.getResource(URL).getFile());
     }
 }
